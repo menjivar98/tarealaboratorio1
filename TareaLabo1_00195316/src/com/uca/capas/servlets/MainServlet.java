1a400bc64ea4022ed3f38adf9743cb7c47c9710e
Fernando <00195316@uca.edu.sv>
2020-03-30 17:00:33 -0600

    TareaLaboratorio1

---
+++ TareaLabo1_00195316/src/com/uca/capas/servlets/MainServlet.java
@@ -0,0 +1,62 @@
+package com.uca.capas.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		PrintWriter out = response.getWriter();
+		
+		if(username.equals("ads") && password.equals("1234")){
+			out.println("<html>");
+			out.println("<body>");
+			out.println("<h1>" + " Acceso Correcto" + "</h1>");
+			out.println("<body>");
+			out.println("<html>");
+		}
+		else {
+			out.println("<html>");
+			out.println("<body>");
+			out.println("<h1>" + " Acceso Incorrecto" + "</h1>");
+			out.println("<body>");
+			out.println("<html>");
+		}
+		
+	}
+
+}
